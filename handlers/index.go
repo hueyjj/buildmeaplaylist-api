d21462eec876f401ea19adff5c17c55f1540469a
Jasper Jeng <jasperjeng@gmail.com>
2019-04-07 12:45:40 -0700

    Return test response for DigitalOcean server

--- handlers/index.go
+++ handlers/index.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 )
 
 // IndexHandler serves default route with homepage
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "GET request OK")
 }
