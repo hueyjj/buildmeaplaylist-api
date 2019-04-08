a0980095def7ca0466c7ff46d93df8a425ceba46
Jasper Jeng <jasperjeng@gmail.com>
2019-04-07 21:18:14 -0700

    Rename Postgres to Controller

--- handlers/database.go
+++ handlers/database.go
@@ -6,19 +6,27 @@ import (
 	"os"
 )
 
-//Postgres holds database driver
-type Postgres struct {
+//Controller holds database driver
+type Controller struct {
 	db *sql.DB
 }
 
+//Member represent in the members table
+type Member struct {
+	Email     string
+	Password  string
+	FirstName string
+	LastName  string
+}
+
 //NewConnection configures and connects to a postgresql database
-func NewConnection(connStr string) *Postgres {
+func NewConnection(connStr string) *Controller {
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
-	return &Postgres{
+	return &Controller{
 		db: db,
 	}
 }
